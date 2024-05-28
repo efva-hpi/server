a03acf5b010b0484d13e8cd207127ed49fb5daf0
Leonard del Federico <leonard.delfederico@gmail.com>
2024-05-28 15:46:58 +0200

    Lobbys erstellbar und beitretbar - auch nicht-existente

--- static/script.js
+++ static/script.js
@@ -1,10 +1,13 @@
 //HTML Elements
 var createLobbyBtn = document.getElementById('createLobbyBtn');
 var createLobbyForm = document.getElementById('createLobbyForm');
-var lobbyCodeInp = document.getElementById('lobbyCodeInp');
-var lobbyCodeInpLabel = document.querySelector('label[for="lobbyCodeInp"]');
+var lobbyCodeHdn = document.getElementById('lobbyCodeHdn');
+var lobbyCodeHdnLabel = document.querySelector('label[for="lobbyCodeHdn"]');
 var cancelLobbyDialogBtn = document.getElementById('cancelLobbyDialogBtn');
 var createLobbyDialog = document.getElementById('createLobbyDialog');
+var joinLobbyForm = document.getElementById('joinLobbyForm');
+var joinLobbyCodeInp = document.getElementById('joinLobbyCodeInp');
+var joinLobbyCodeInpLabel = document.querySelector('label[for="joinLobbyCodeInp"]');
 //Lobby vars
 var lobbyCode;
 //Event listeners
@@ -12,8 +15,8 @@ createLobbyBtn === null || createLobbyBtn === void 0 ? void 0 : createLobbyBtn.a
     console.log("createLobbyBtn clicked");
     createLobbyDialog.showModal();
     lobbyCode = Math.random().toString(36).substring(2, 8).toUpperCase();
-    lobbyCodeInp.value = lobbyCode;
-    lobbyCodeInpLabel.innerHTML = "Lobby entry code: ".concat(lobbyCode);
+    lobbyCodeHdn.value = lobbyCode;
+    lobbyCodeHdnLabel.innerHTML = "Lobby entry code: ".concat(lobbyCode);
 });
 cancelLobbyDialogBtn === null || cancelLobbyDialogBtn === void 0 ? void 0 : cancelLobbyDialogBtn.addEventListener('click', function () {
     createLobbyDialog.close();
@@ -21,5 +24,15 @@ cancelLobbyDialogBtn === null || cancelLobbyDialogBtn === void 0 ? void 0 : canc
 function getFormAction(form) {
     if (form === createLobbyForm) {
         form.action = "/lobby/".concat(lobbyCode);
+    }
+    if (form === joinLobbyForm) {
+        var joinCode = joinLobbyCodeInp.value;
+        if (/^([A-Z0-9]){6}$/.test(joinCode)) {
+            form.action = "/lobby/".concat(joinCode);
+            return true;
+        }
+        joinLobbyCodeInpLabel.innerHTML = "Bitte einen gültigen Lobby code eingeben.<br>";
+        form.action = "";
+        return false;
     }
 }
--- static/script.ts
+++ static/script.ts
@@ -1,10 +1,13 @@
 //HTML Elements
 const createLobbyBtn = document.getElementById('createLobbyBtn');
 const createLobbyForm = document.getElementById('createLobbyForm');
-const lobbyCodeInp = <HTMLInputElement> document.getElementById('lobbyCodeInp');
-const lobbyCodeInpLabel = <HTMLLabelElement> document.querySelector('label[for="lobbyCodeInp"]');
+const lobbyCodeHdn = <HTMLInputElement> document.getElementById('lobbyCodeHdn');
+const lobbyCodeHdnLabel = <HTMLLabelElement> document.querySelector('label[for="lobbyCodeHdn"]');
 const cancelLobbyDialogBtn = document.getElementById('cancelLobbyDialogBtn');
 const createLobbyDialog = <HTMLDialogElement>document.getElementById('createLobbyDialog');
+const joinLobbyForm = document.getElementById('joinLobbyForm');
+const joinLobbyCodeInp = <HTMLInputElement> document.getElementById('joinLobbyCodeInp');
+const joinLobbyCodeInpLabel = <HTMLLabelElement> document.querySelector('label[for="joinLobbyCodeInp"]');
 
 //Lobby vars
 var lobbyCode: string;
@@ -15,8 +18,8 @@ createLobbyBtn?.addEventListener('click', () => {
     console.log("createLobbyBtn clicked");
     createLobbyDialog.showModal();
     lobbyCode = Math.random().toString(36).substring(2, 8).toUpperCase();
-    lobbyCodeInp.value = lobbyCode;
-    lobbyCodeInpLabel.innerHTML = `Lobby entry code: ${lobbyCode}`;
+    lobbyCodeHdn.value = lobbyCode;
+    lobbyCodeHdnLabel.innerHTML = `Lobby entry code: ${lobbyCode}`;
 })
 
 cancelLobbyDialogBtn?.addEventListener('click', () => {
@@ -26,5 +29,15 @@ cancelLobbyDialogBtn?.addEventListener('click', () => {
 function getFormAction(form: HTMLFormElement){
     if(form === createLobbyForm){
         form.action = `/lobby/${lobbyCode}`;
+    }
+    if(form === joinLobbyForm){
+        let joinCode: string = joinLobbyCodeInp.value;
+        if(/^([A-Z0-9]){6}$/.test(joinCode)){
+            form.action = `/lobby/${joinCode}`;
+            return true;
+        }
+        joinLobbyCodeInpLabel.innerHTML = "Bitte einen gültigen Lobby code eingeben.<br>";
+        form.action="";
+        return false;
     }
 }
