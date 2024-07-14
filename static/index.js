01c527aa27d3bfde25544c0e756b582e523f5a4c
Yven Exner <john2004-06@web.de>
2024-07-14 15:58:59 +0200

    login design

--- static/index.js
+++ static/index.js
@@ -3,12 +3,16 @@
 var loginBtn = document.getElementById('loginBtn');
 var loginDialog = document.getElementById('loginDialog');
 var loginForm = document.getElementById('loginForm');
+var cancelLoginDialogBtn = document.getElementById('cancelLoginDialogBtn');
+
 // Registrierung
 var registerBtn = document.getElementById('registerBtn');
 var registerDialog = document.getElementById('registerDialog');
 var registerForm = document.getElementById('registerForm');
+var cancelRegisterDialogBtn = document.getElementById('cancelRegisterDialogBtn');
 var registerPasswordInp = document.getElementById('registerPasswordInp');
 var confirmPasswordInp = document.getElementById('confirmPasswordInp');
+
 // Lobby erstellen
 var createLobbyBtn = document.getElementById('createLobbyBtn');
 var createLobbyForm = document.getElementById('createLobbyForm');
@@ -19,31 +23,46 @@ var createLobbyDialog = document.getElementById('createLobbyDialog');
 var joinLobbyForm = document.getElementById('joinLobbyForm');
 var joinLobbyCodeInp = document.getElementById('joinLobbyCodeInp');
 var joinLobbyCodeInpLabel = document.querySelector('label[for="joinLobbyCodeInp"]');
+
 // Error dialog
 var errorDialog = document.getElementById('errorDialog');
 var closeErrorDialogBtn = document.getElementById('closeErrorDialogBtn');
-//Lobby vars
+
+// Lobby vars
 var lobbyCode;
-//Event listeners
+
+// Event listeners
 createLobbyBtn === null || createLobbyBtn === void 0 ? void 0 : createLobbyBtn.addEventListener('click', function () {
-    // console.log("createLobbyBtn clicked");
     createLobbyDialog.showModal();
     lobbyCode = Math.random().toString(36).substring(2, 8).toUpperCase();
     lobbyCodeHdn.value = lobbyCode;
     lobbyCodeHdnLabel.innerHTML = "Lobby entry code: ".concat(lobbyCode);
 });
+
 cancelLobbyDialogBtn === null || cancelLobbyDialogBtn === void 0 ? void 0 : cancelLobbyDialogBtn.addEventListener('click', function () {
     createLobbyDialog.close();
 });
+
 loginBtn === null || loginBtn === void 0 ? void 0 : loginBtn.addEventListener('click', function () {
     loginDialog.showModal();
 });
+
+cancelLoginDialogBtn === null || cancelLoginDialogBtn === void 0 ? void 0 : cancelLoginDialogBtn.addEventListener('click', function () {
+    loginDialog.close();
+});
+
 registerBtn === null || registerBtn === void 0 ? void 0 : registerBtn.addEventListener('click', function () {
     registerDialog.showModal();
 });
+
+cancelRegisterDialogBtn === null || cancelRegisterDialogBtn === void 0 ? void 0 : cancelRegisterDialogBtn.addEventListener('click', function () {
+    registerDialog.close();
+});
+
 closeErrorDialogBtn === null || closeErrorDialogBtn === void 0 ? void 0 : closeErrorDialogBtn.addEventListener('click', function () {
     errorDialog.close();
 });
+
 function getFormAction(form) {
     switch (form) {
         case createLobbyForm:
@@ -61,15 +80,13 @@ function getFormAction(form) {
             return true;
     }
 }
+
 registerPasswordInp.onchange = validatePassword;
 confirmPasswordInp.onkeyup = validatePassword;
+
 // Functions
 function validatePassword() {
     if (registerPasswordInp.value != confirmPasswordInp.value) {
-        confirmPasswordInp.setCustomValidity("Passwords Don't Match");
-        return false;
-    }
-    else {
-        confirmPasswordInp.setCustomValidity('');
+        confirmPasswordInp.set
     }
 }
--- static/index.ts
+++ static/index.ts
@@ -3,33 +3,38 @@
 const loginBtn = document.getElementById('loginBtn');
 const loginDialog = <HTMLDialogElement>document.getElementById('loginDialog');
 const loginForm = document.getElementById('loginForm');
+const cancelLoginDialogBtn = document.getElementById('cancelLoginDialogBtn');
+
 // Registrierung
 const registerBtn = document.getElementById('registerBtn');
 const registerDialog = <HTMLDialogElement>document.getElementById('registerDialog');
 const registerForm = document.getElementById('registerForm');
+const cancelRegisterDialogBtn = document.getElementById('cancelRegisterDialogBtn');
 const registerPasswordInp = <HTMLInputElement>document.getElementById('registerPasswordInp');
 const confirmPasswordInp = <HTMLInputElement>document.getElementById('confirmPasswordInp');
+
 // Lobby erstellen
 const createLobbyBtn = document.getElementById('createLobbyBtn');
 const createLobbyForm = document.getElementById('createLobbyForm');
-const lobbyCodeHdn = <HTMLInputElement> document.getElementById('lobbyCodeHdn');
-const lobbyCodeHdnLabel = <HTMLLabelElement> document.querySelector('label[for="lobbyCodeHdn"]');
+const lobbyCodeHdn = <HTMLInputElement>document.getElementById('lobbyCodeHdn');
+const lobbyCodeHdnLabel = <HTMLLabelElement>document.querySelector('label[for="lobbyCodeHdn"]');
 const cancelLobbyDialogBtn = document.getElementById('cancelLobbyDialogBtn');
 const createLobbyDialog = <HTMLDialogElement>document.getElementById('createLobbyDialog');
+
+// Lobby beitreten
 const joinLobbyForm = document.getElementById('joinLobbyForm');
-const joinLobbyCodeInp = <HTMLInputElement> document.getElementById('joinLobbyCodeInp');
-const joinLobbyCodeInpLabel = <HTMLLabelElement> document.querySelector('label[for="joinLobbyCodeInp"]');
+const joinLobbyCodeInp = <HTMLInputElement>document.getElementById('joinLobbyCodeInp');
+const joinLobbyCodeInpLabel = <HTMLLabelElement>document.querySelector('label[for="joinLobbyCodeInp"]');
+
 // Error dialog
 const errorDialog = <HTMLDialogElement>document.getElementById('errorDialog');
 const closeErrorDialogBtn = document.getElementById('closeErrorDialogBtn');
 
-//Lobby vars
+// Lobby vars
 var lobbyCode: string;
 
-
-//Event listeners
+// Event listeners
 createLobbyBtn?.addEventListener('click', () => {
-    // console.log("createLobbyBtn clicked");
     createLobbyDialog.showModal();
     lobbyCode = Math.random().toString(36).substring(2, 8).toUpperCase();
     lobbyCodeHdn.value = lobbyCode;
@@ -44,15 +49,23 @@ loginBtn?.addEventListener('click', () => {
     loginDialog.showModal();
 });
 
+cancelLoginDialogBtn?.addEventListener('click', () => {
+    loginDialog.close();
+});
+
 registerBtn?.addEventListener('click', () => {
     registerDialog.showModal();
+});
+
+cancelRegisterDialogBtn?.addEventListener('click', () => {
+    registerDialog.close();
 });
 
 closeErrorDialogBtn?.addEventListener('click', () => {
     errorDialog.close();
 });
 
-function getFormAction(form: HTMLFormElement){
+function getFormAction(form: HTMLFormElement) {
     switch (form) {
         case createLobbyForm:
             form.action = `/lobby/${lobbyCode}`;
