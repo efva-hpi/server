a015afe7fec739ab82de80124dbf9fca5fd8f8ae
Leonard del Federico <leonard.delfederico@gmail.com>
2024-07-16 14:30:51 +0200

    TS compiler changed compilation

--- static/index.js
+++ static/index.js
@@ -4,7 +4,6 @@ var loginBtn = document.getElementById('loginBtn');
 var loginDialog = document.getElementById('loginDialog');
 var loginForm = document.getElementById('loginForm');
 var cancelLoginDialogBtn = document.getElementById('cancelLoginDialogBtn');
-
 // Registrierung
 var registerBtn = document.getElementById('registerBtn');
 var registerDialog = document.getElementById('registerDialog');
@@ -12,7 +11,6 @@ var registerForm = document.getElementById('registerForm');
 var cancelRegisterDialogBtn = document.getElementById('cancelRegisterDialogBtn');
 var registerPasswordInp = document.getElementById('registerPasswordInp');
 var confirmPasswordInp = document.getElementById('confirmPasswordInp');
-
 // Lobby erstellen
 var createLobbyBtn = document.getElementById('createLobbyBtn');
 var createLobbyForm = document.getElementById('createLobbyForm');
@@ -20,17 +18,15 @@ var lobbyCodeHdn = document.getElementById('lobbyCodeHdn');
 var lobbyCodeHdnLabel = document.querySelector('label[for="lobbyCodeHdn"]');
 var cancelLobbyDialogBtn = document.getElementById('cancelLobbyDialogBtn');
 var createLobbyDialog = document.getElementById('createLobbyDialog');
+// Lobby beitreten
 var joinLobbyForm = document.getElementById('joinLobbyForm');
 var joinLobbyCodeInp = document.getElementById('joinLobbyCodeInp');
 var joinLobbyCodeInpLabel = document.querySelector('label[for="joinLobbyCodeInp"]');
-
 // Error dialog
 var errorDialog = document.getElementById('errorDialog');
 var closeErrorDialogBtn = document.getElementById('closeErrorDialogBtn');
-
 // Lobby vars
 var lobbyCode;
-
 // Event listeners
 createLobbyBtn === null || createLobbyBtn === void 0 ? void 0 : createLobbyBtn.addEventListener('click', function () {
     createLobbyDialog.showModal();
@@ -38,31 +34,24 @@ createLobbyBtn === null || createLobbyBtn === void 0 ? void 0 : createLobbyBtn.a
     lobbyCodeHdn.value = lobbyCode;
     lobbyCodeHdnLabel.innerHTML = "Lobby entry code: ".concat(lobbyCode);
 });
-
 cancelLobbyDialogBtn === null || cancelLobbyDialogBtn === void 0 ? void 0 : cancelLobbyDialogBtn.addEventListener('click', function () {
     createLobbyDialog.close();
 });
-
 loginBtn === null || loginBtn === void 0 ? void 0 : loginBtn.addEventListener('click', function () {
     loginDialog.showModal();
 });
-
 cancelLoginDialogBtn === null || cancelLoginDialogBtn === void 0 ? void 0 : cancelLoginDialogBtn.addEventListener('click', function () {
     loginDialog.close();
 });
-
 registerBtn === null || registerBtn === void 0 ? void 0 : registerBtn.addEventListener('click', function () {
     registerDialog.showModal();
 });
-
 cancelRegisterDialogBtn === null || cancelRegisterDialogBtn === void 0 ? void 0 : cancelRegisterDialogBtn.addEventListener('click', function () {
     registerDialog.close();
 });
-
 closeErrorDialogBtn === null || closeErrorDialogBtn === void 0 ? void 0 : closeErrorDialogBtn.addEventListener('click', function () {
     errorDialog.close();
 });
-
 function getFormAction(form) {
     switch (form) {
         case createLobbyForm:
@@ -80,13 +69,15 @@ function getFormAction(form) {
             return true;
     }
 }
-
 registerPasswordInp.onchange = validatePassword;
 confirmPasswordInp.onkeyup = validatePassword;
-
 // Functions
 function validatePassword() {
     if (registerPasswordInp.value != confirmPasswordInp.value) {
-        confirmPasswordInp.set
+        confirmPasswordInp.setCustomValidity("Passwords Don't Match");
+        return false;
+    }
+    else {
+        confirmPasswordInp.setCustomValidity('');
     }
 }
