6c788c5bb2aaf71cab12f0ee22d7b28bcfe7583b
ediiiiiiii <edgar.bennemann@student.hpi.uni-potsdam.de>
2024-07-16 22:03:22 +0200

    Added qr code on lobby page

--- static/lobby.js
+++ static/lobby.js
@@ -1,6 +1,11 @@
 // HTML Elements
 var leaveBtn = document.getElementById("leaveBtn");
 // Event listeners
+// update qr code
+var qrcode = document.getElementById("qrcode");
+var url = "https://api.qrserver.com/v1/create-qr-code/?data=" + window.location.href + "&amp;size=150x150";
+qrcode.src = url;
+console.log(url);
 //leave lobby
 leaveBtn === null || leaveBtn === void 0 ? void 0 : leaveBtn.addEventListener('click', function () {
     window.location.href = "".concat(window.location.pathname, "/leave");
--- static/lobby.ts
+++ static/lobby.ts
@@ -3,6 +3,12 @@ const leaveBtn = document.getElementById("leaveBtn");
 
 // Event listeners
 
+// update qr code
+let qrcode = document.getElementById("qrcode") as HTMLImageElement;
+let url = "https://api.qrserver.com/v1/create-qr-code/?data=" + window.location.href + "&amp;size=150x150";
+qrcode.src = url;
+console.log(url);
+
 //leave lobby
 leaveBtn?.addEventListener('click', () => {
     window.location.href = `${window.location.pathname}/leave`;
