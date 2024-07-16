4326afd8b0fe8752ffb4f23361eb1b0695ded571
Leonard del Federico <leonard.delfederico@gmail.com>
2024-07-16 16:23:01 +0200

    Creating and joining a lobby now only possible when logged in. Creates and adds Player object when joining lobby, removes that player when leaving

--- app.py
+++ app.py
@@ -10,6 +10,8 @@ app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 gs: GameState = GameState()
+
+
 # validLobbyKey: bool = True
 
 @app.route("/")
@@ -19,34 +21,57 @@ def main_page():
         session.pop('auth_token')
     return render_template("index.html", auth_token=auth_token)
 
+
 @app.route("/lobby/<code>", methods=["GET", "POST"])
 def lobby(code):
     session.pop('_flashes', None)
+    auth_token: str = request.cookies.get('auth_token', None)
+    if not auth_token:
+        flash('Please log in or register to play', 'error')
+        return redirect("/")
+    decoded_auth_token = decode_token(auth_token)
+    username = decoded_auth_token['username']
     global validLobbyKey
     if request.method == "POST":
         # print("lobby post")
         form = request.form
-        if (form.get("lobbyFormType") == "create"):
+        if form.get("lobbyFormType") == "create":
             # print("creating lobby")
-            game_settings = GameSettings(form.get("numberOfQuestions"), form.get("category"), form.get("difficulty"), form.get("gamemode"))
+            game_settings = GameSettings(
+                int(form.get("numberOfQuestions")),
+                int(form.get("category")),
+                form.get("difficulty"),
+                form.get("gamemode")
+            )
             gs.create_lobby(code, game_settings)
-            return redirect(f"/lobby/{code}")
-        elif (form.get("lobbyFormType") == "join"):
-            # print("joining lobby")
-            currentLobby = gs.get_lobby_by_code(code)
-            if(currentLobby is None):
-                return no_valid_lobby()
-            return redirect(f"/lobby/{code}")
-    elif (request.method == "GET"):
-        # print("lobby get")
-        currentLobby = gs.get_lobby_by_code(code)
-        if(currentLobby is None):
+
+        currentLobby: Lobby = gs.get_lobby_by_code(code)
+        if currentLobby is None:
             return no_valid_lobby()
-        return render_template("lobby.html", lobbyInfo = currentLobby.game_settings, lobbyCode = currentLobby.code)
+        joining_player: Player = Player(username)
+        currentLobby.add_player(joining_player)
+        return redirect(f"/lobby/{code}")
+    elif request.method == "GET":
+        # print("lobby get")
+        current_lobby = gs.get_lobby_by_code(code)
+        if current_lobby is None:
+            return no_valid_lobby()
+        players: list[str] = current_lobby.get_player_list()
+        return render_template("lobby.html", lobbyInfo=current_lobby.game_settings, lobbyCode=current_lobby.code, players=players)
+
 
 @app.route("/lobby/<code>/leave", methods=["GET", "POST"])
 def leave_lobby(code):
+    auth_token = request.cookies.get('auth_token', None)
+    if not auth_token:
+        redirect("/")
+    leaving_player_username = decode_token(auth_token)['username']
+    current_lobby: Lobby = gs.get_lobby_by_code(code)
+    players: list[Player] = current_lobby.get_players()
+    leaving_player: Player = next((player for player in players if player.username == leaving_player_username), None)
+    current_lobby.remove_player(leaving_player)
     return redirect("/")
+
 
 @app.route("/login", methods=["POST"])
 def login():
@@ -55,14 +80,15 @@ def login():
         username = form.get("loginUsernameInp")
         password = form.get("loginPasswordInp")
         if not a_login(username, password):
-            flash('username oder Passwort ist falsch', error)
+            flash('username oder Passwort ist falsch', 'error')
             return redirect("/")
-        token = generateToken(username)
+        token = generate_token(username)
         session['auth_token'] = token
         return redirect("/")
     except psycopg2Error as e:
         flash('Kein Nutzer unter diesen Daten gefunden', 'error')
         return redirect("/")
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -71,12 +97,13 @@ def register():
             form = request.form
             username = form.get("registerUsernameInp")
             a_register(username, form.get("registerPasswordInp"), form.get("registerEmailInp"))
-            token = generateToken(username)
+            token = generate_token(username)
             session['auth_token'] = token
             return redirect("/")
         except psycopg2Error as e:
             flash('Dieser Nutzer hat bereits ein Konto', 'error')
             return redirect("/")
+
 
 # Hilfsfunktionen
 def no_valid_lobby():
@@ -84,8 +111,12 @@ def no_valid_lobby():
     flash('Bitte einen gültigen Lobby code eingeben', 'error')
     return redirect("/")
 
-def generateToken(username: str):
+
+def generate_token(username: str):
     return jwt.encode({
-            'username': username,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=3)
-        }, app.secret_key, algorithm='HS256')
+        'username': username,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(days=3)
+    }, app.secret_key, algorithm='HS256')
+
+def decode_token(token: str):
+    return jwt.decode(token.encode('UTF-8'), app.secret_key, algorithms=['HS256'])
