8d639e32b38b748dae081020f2259532fa1c07f1
Leonard del Federico <leonard.delfederico@gmail.com>
2024-07-16 14:31:16 +0200

    Loging in or registering generates a JWT token to authenticate the user

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask, render_template, request, redirect, flash, session
 from markupsafe import escape
 from spiellogik import *
 from login import login as a_login, register as a_register, psycopg2Error
-
+import jwt
+import datetime
 
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
@@ -13,8 +14,10 @@ gs: GameState = GameState()
 
 @app.route("/")
 def main_page():
-    print(session.get('_flashes', None))
-    return render_template("index.html")
+    auth_token = session.get('auth_token', None)
+    if auth_token:
+        session.pop('auth_token')
+    return render_template("index.html", auth_token=auth_token)
 
 @app.route("/lobby/<code>", methods=["GET", "POST"])
 def lobby(code):
@@ -33,28 +36,32 @@ def lobby(code):
             currentLobby = gs.get_lobby_by_code(code)
             if(currentLobby is None):
                 return no_valid_lobby()
-            print("lobby found")
             return redirect(f"/lobby/{code}")
     elif (request.method == "GET"):
         # print("lobby get")
         currentLobby = gs.get_lobby_by_code(code)
         if(currentLobby is None):
             return no_valid_lobby()
-        print("rendering lobby get")
         return render_template("lobby.html", lobbyInfo = currentLobby.game_settings, lobbyCode = currentLobby.code)
 
 @app.route("/lobby/<code>/leave", methods=["GET", "POST"])
 def leave_lobby(code):
-    print("leaving lobby")
     return redirect("/")
 
 @app.route("/login", methods=["POST"])
 def login():
     try:
         form = request.form
-        a_login(form.get("loginUsernameInp"), form.get("loginPasswordInp"))
+        username = form.get("loginUsernameInp")
+        password = form.get("loginPasswordInp")
+        if not a_login(username, password):
+            flash('username oder Passwort ist falsch', error)
+            return redirect("/")
+        token = generateToken(username)
+        session['auth_token'] = token
         return redirect("/")
     except psycopg2Error as e:
+        flash('Kein Nutzer unter diesen Daten gefunden', 'error')
         return redirect("/")
 
 @app.route("/register", methods=["GET", "POST"])
@@ -62,7 +69,10 @@ def register():
     if request.method == "POST":
         try:
             form = request.form
-            a_register(form.get("registerUsernameInp"), form.get("registerPasswordInp"), form.get("registerEmailInp"))
+            username = form.get("registerUsernameInp")
+            a_register(username, form.get("registerPasswordInp"), form.get("registerEmailInp"))
+            token = generateToken(username)
+            session['auth_token'] = token
             return redirect("/")
         except psycopg2Error as e:
             flash('Dieser Nutzer hat bereits ein Konto', 'error')
@@ -73,3 +83,9 @@ def no_valid_lobby():
     # print("no valid lobby")
     flash('Bitte einen gültigen Lobby code eingeben', 'error')
     return redirect("/")
+
+def generateToken(username: str):
+    return jwt.encode({
+            'username': username,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=3)
+        }, app.secret_key, algorithm='HS256')
