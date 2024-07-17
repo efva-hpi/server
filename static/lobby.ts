69c619f5ed48609ec2794ae255dd04faf14c58d0
Leonard del Federico <leonard.delfederico@gmail.com>
2024-07-17 16:32:40 +0200

    Added WebSocketIO basics

--- static/lobby.ts
+++ static/lobby.ts
@@ -1,3 +1,5 @@
+import io from 'socket.io-client';
+
 // HTML Elements
 const leaveBtn = document.getElementById("leaveBtn");
 const messageDialog = <HTMLDialogElement> document.getElementById('messageDialog');
@@ -5,22 +7,28 @@ const guestUsernameInp = document.getElementById('guestUsernameInp');
 const submitUsernameBtn = document.getElementById('submitUsernameBtn');
 
 // WebSockets
-const href = window.location.href;
-const lobbySocket: WebSocket = new WebSocket(href);
-
-const currentUrl: string = href.slice(0, -12);
-const currentLobbyCode: string = href.slice(-6);
-const gameUrl: string = `${currentUrl}game/${currentLobbyCode}`;
-const gameSocket: WebSocket = new WebSocket(gameUrl);
+let href = window.location.href;
+href = href.substring(href.indexOf(':'));
+// console.log(`ws${href}`);
+// const lobbySocket: WebSocket = new WebSocket(`ws${href}`);
+//
+// const currentUrl: string = href.slice(0, -12);
+// const currentLobbyCode: string = href.slice(-6);
+// const gameUrl: string = `ws${currentUrl}game/${currentLobbyCode}`;
+// const gameSocket: WebSocket = new WebSocket(gameUrl);
+const socket = io();
+socket.on('connect', () => {
+   socket.emit('message', { data: 'Hello World!' });
+});
 
 // Event listeners
-lobbySocket.addEventListener("open", (event: Event): void => {
-    lobbySocket.send("Hello Server!")
-});
-
-lobbySocket.addEventListener("message", (event: MessageEvent): void => {
-    console.log(`Message from Server: ${event.data}`)
-});
+// lobbySocket.addEventListener("open", (event: Event): void => {
+//     lobbySocket.send("Hello Server!")
+// });
+//
+// lobbySocket.addEventListener("message", (event: MessageEvent): void => {
+//     console.log(`Message from Server: ${event.data}`)
+// });
 
 // update qr code
 let qrcode = document.getElementById("qrcode") as HTMLImageElement;
